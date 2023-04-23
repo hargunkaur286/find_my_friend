6bd3e67080cddcb21ce50c83d52330e4a07c99e3
priyal <priyaljaiin52@gmail.com>
2023-04-23 14:19:10 +0530

    profile buttons enabled

--- src/components/Cardss.jsx
+++ src/components/Cardss.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+
 import Typography from '@mui/material/Typography';
 import sample from '../assets/sample.jpeg';
 import {Stack} from '@mui/material'
